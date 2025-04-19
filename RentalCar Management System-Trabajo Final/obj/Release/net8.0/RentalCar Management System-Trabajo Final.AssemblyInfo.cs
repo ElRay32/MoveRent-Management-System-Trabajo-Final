ffc080f2e335f025555934d81959e7f1b9f6d439
ElRay32 <guerrero20191@outlook.es>
2025-04-19 17:47:47 -0400

    Creacion Conexion de la BD

--- RentalCar Management System-Trabajo Final/obj/Release/net8.0/RentalCar Management System-Trabajo Final.AssemblyInfo.cs
+++ RentalCar Management System-Trabajo Final/obj/Release/net8.0/RentalCar Management System-Trabajo Final.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentalCar Management System-Trabajo Final")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8aeeb4b6ef079546fb2f4cdbc7149cf8c09ff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentalCar Management System-Trabajo Final")]
