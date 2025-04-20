7418ea52e6e2e29f63ca573a53f9baacbbed91d2
ElRay32 <guerrero20191@outlook.es>
2025-04-20 01:16:06 -0400

    Mejoras

---
+++ RentalCar Management System/Program.cs
@@ -0,0 +1,17 @@
+﻿using RentalCar.BLL;
+using RentalCar.Models;
+
+
+namespace RentalCar.interf
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            AutoService autoService = new AutoService();
+            Auto auto = new Auto(0, "Toyota", "Corolla", "A123BCD", true);
+            autoService.AgregarAuto(auto);
+            Console.WriteLine("Auto agregado exitosamente.");
+        }
+    }
+}
---
+++ RentalCar/BDConexion/ConexionDB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace RentalCar.DAL
+{
+    public class ConexionDB
+    {
+        private readonly string cadenaConexion = "Data Source=.;Initial Catalog=RentalCarDB;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;";
+
+        public SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(cadenaConexion);
+        }
+    }
+}
